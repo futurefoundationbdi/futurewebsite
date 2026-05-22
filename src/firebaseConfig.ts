577e803bc0057ccdd7bb1316624d25df66988e8f
futurefoundationbdi <thefuturefoundationbdi@gmail.com>
2026-05-22 21:01:56 +0200

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // <-- AJOUTÉ
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBQjj-rk-GW0jgGb27gdaKhDxhty5Uv9po",
   authDomain: "futuregeneration-web.firebaseapp.com",
@@ -19,3 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and export it
+export const db = getFirestore(app); // <-- INDISPENSABLE POUR LIBRARY.TSX
